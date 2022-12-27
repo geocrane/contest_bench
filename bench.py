beb4ca65bea01405e058af891e4f385209000ee9
Сергей Журавлев <geo-rnd@yandex.ru>
2022-12-27 09:26:36 +0300

    init

--- bench.py
+++ bench.py
@@ -1,14 +1,1 @@
-def get_score(keys, all_symbols, valid_characters="123456789", players=2):
-    return sum(
-        0 != all_symbols.count(character) <= players * keys
-        for character in valid_characters
-    )
 
-
-if __name__ == "__main__":
-    print(
-        get_score(
-            keys=int(input()),
-            all_symbols=f"{input()}{input()}{input()}{input()}",
-        )
-    )
