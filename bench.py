669e9589daa5600359d27642f89d9e82e3c8ee02
Сергей Журавлев <geo-rnd@yandex.ru>
2022-12-27 09:14:26 +0300

    init

--- bench.py
+++ bench.py
@@ -1,14 +1,22 @@
-def get_score(keys, all_symbols, valid_characters="123456789", players=2):
-    return sum(
-        0 != all_symbols.count(character) <= players * keys
-        for character in valid_characters
-    )
+# Здесь писать код решения задачи. В качестве примера решение "Ближайший ноль"
+
+def get_distances(houses, empty="0"):
+    zeros = [index for index, number in enumerate(houses) if number == empty]
+    distances = [0] * len(houses)
+    first, last = zeros[0], zeros[-1]
+    for left, right in zip(zeros, zeros[1:]):
+        distances[left + 1:right] = [
+            min(position - left, right - position)
+            for position in range(left + 1, right)
+        ]
+    distances[:first] = [first - position for position in range(first)]
+    distances[last + 1:] = [
+        position - last for position in range(last + 1, len(distances))
+    ]
+    return distances
 
 
 if __name__ == "__main__":
-    print(
-        get_score(
-            keys=int(input()),
-            all_symbols=f"{input()}{input()}{input()}{input()}",
-        )
-    )
+
+    input()
+    print(*get_distances(houses=input().split()))
